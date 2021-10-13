822e63813ba029a0c3ac4eff69c6e904f66e7fc6
kukumaj <zakumaj@gmail.com>
2021-10-13 22:12:42 +0200

    ok

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,25 +5,29 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-String jsoon = "{\n" +
-        "  \"tree\": {\n" +
-        "    \"nodes\": [\n" +
-        "      {\"id\": \"1\", \"left\": \"2\", \"right\": \"3\", \"value\": 1},\n" +
-        "      {\"id\": \"2\", \"left\": \"4\", \"right\": \"5\", \"value\": 2},\n" +
-        "      {\"id\": \"3\", \"left\": \"6\", \"right\": \"7\", \"value\": 3},\n" +
-        "      {\"id\": \"4\", \"left\": \"8\", \"right\": \"9\", \"value\": 4},\n" +
-        "      {\"id\": \"5\", \"left\": \"10\", \"right\": null, \"value\": 5},\n" +
-        "      {\"id\": \"6\", \"left\": null, \"right\": null, \"value\": 6},\n" +
-        "      {\"id\": \"7\", \"left\": null, \"right\": null, \"value\": 7},\n" +
-        "      {\"id\": \"8\", \"left\": null, \"right\": null, \"value\": 8},\n" +
-        "      {\"id\": \"9\", \"left\": null, \"right\": null, \"value\": 9},\n" +
-        "      {\"id\": \"10\", \"left\": null, \"right\": null, \"value\": 10}\n" +
-        "    ],\n" +
-        "    \"root\": \"1\"\n" +
-        "  }\n" +
-        "}";
+        String json = "{" +
+                "  \"tree\": {" +
+                "    \"nodes\": [" +
+                "      {\"id\": \"1\", \"left\": \"2\", \"right\": \"3\", \"value\": 1}," +
+                "      {\"id\": \"2\", \"left\": \"4\", \"right\": \"5\", \"value\": 2}," +
+                "      {\"id\": \"3\", \"left\": \"6\", \"right\": \"7\", \"value\": 3}," +
+                "      {\"id\": \"4\", \"left\": \"8\", \"right\": \"9\", \"value\": 4}," +
+                "      {\"id\": \"5\", \"left\": \"10\", \"right\": null, \"value\": 5}," +
+                "      {\"id\": \"6\", \"left\": null, \"right\": null, \"value\": 6}," +
+                "      {\"id\": \"7\", \"left\": null, \"right\": null, \"value\": 7}," +
+                "      {\"id\": \"8\", \"left\": null, \"right\": null, \"value\": 8}," +
+                "      {\"id\": \"9\", \"left\": null, \"right\": null, \"value\": 9}," +
+                "      {\"id\": \"10\", \"left\": null, \"right\": null, \"value\": 10}" +
+                "    ]," +
+                "    \"root\": \"1\"" +
+                "  }" +
+                "}";
+
+
+
+
         ObjectMapper objectMapper = new ObjectMapper();
-        TreeObject treeObject = objectMapper.readValue(jsoon,TreeObject.class);
+        TreeObject treeObject = objectMapper.readValue(json,TreeObject.class);
 
     }
 }
--- src/main/java/TreeObject.java
+++ src/main/java/TreeObject.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+
 public class TreeObject {
-    Tree tree;
+    private Tree tree;
+
+    public Tree getTree() {
+        return tree;
+    }
+
+    public void setTree(Tree tree) {
+
+        this.tree = tree;
+    }
+    public ArrayList<Integer> countBranches() {
+      return null;
+    }
 }
